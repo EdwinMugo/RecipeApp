4782d442138ffd2ddba70b4fc4a539a65d3e53c2
EdwinMugo <edwinmurgoh@gmail.com>
2024-09-05 18:55:02 +0300

    second branch

--- Recipe-app/src/components/Nav.jsx
+++ Recipe-app/src/components/Nav.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
 
 export default function NavBar(){
     return (
         <>
+        <Link to={'/'}> 
         <h1> 🍟 Recipe APP</h1>
+        </Link>
         <nav> 
             
         </nav>
